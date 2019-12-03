28848b1b605905bda2206e3b5688dae1f3104ec1
Vitalii Sotnichenko <vitalii.sotnichenko@Vitaliis-MacBook-Pro.local>
2019-12-03 15:40:05 +0200

    Add base class

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,9 @@
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+class MainPage():
+
+    def test_guest_can_go_to_login_page(browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        browser.get(link)
+        login_link = browser.find_element_by_css_selector("#login_link")
+        login_link.click()
